033ce70aaf9ee2ffb4811be49e21702177e63c0f
Soumya Verma <soumya.verma2000@gmail.com>
2021-06-04 17:59:10 +0530

    Final commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -66,7 +66,7 @@ router.post(
       jwt.sign(
         tokendata,
         config.get("jwtSecret"),
-        { expiresIn: 360000 },
+        { expiresIn: 3600 },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
